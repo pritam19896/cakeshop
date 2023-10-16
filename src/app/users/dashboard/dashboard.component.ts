a35798c6d7144a6c0ad26ff654db031b0c4214ad
Pritam Das <pritamdas@Pritams-MacBook-Air.local>
2023-10-17 00:56:35 +0530

    added css for varity of cakes

--- src/app/users/dashboard/dashboard.component.ts
+++ src/app/users/dashboard/dashboard.component.ts
@@ -1,29 +1,42 @@
-import { Component } from '@angular/core';
+      import { Component, HostListener } from '@angular/core';
 
-@Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
-})
-export class DashboardComponent {
+      @Component({
+      selector: 'app-dashboard',
+      templateUrl: './dashboard.component.html',
+      styleUrls: ['./dashboard.component.scss']
+      })
+      export class DashboardComponent {
 
-  images = [
-    {
-      path:'../../../assets/mainimg/custom.jpeg',
-      picHeader: 'Custom Cakes'
-    },
-    {
-      path:'../../../assets/mainimg/farewell.jpeg',
-      picHeader: 'Farewell Cakes'
-    },
-    {
-      path:'../../../assets/mainimg/pastries.jpeg',
-      picHeader: 'Mouth Watering Pastries'
-    }, 
-    {
-      path:'../../../assets/mainimg/birthday.jpeg',
-      picHeader: 'Birthday Cakes'
-    },
-  ];
-  
+        public innerWidth: any;
+ngOnInit() {
+    this.innerWidth = window.innerWidth;
 }
+
+@HostListener('window:resize', ['$event'])
+onResize() {
+  this.innerWidth = window.innerWidth;
+  if(window.innerWidth <= 770){
+    this.innerWidth = true;
+  }
+}
+
+      images = [
+      {
+        path:'../../../assets/mainimg/custom.jpeg',
+        picHeader: 'Custom Cakes'
+      },
+      {
+        path:'../../../assets/mainimg/farewell.jpeg',
+        picHeader: 'Farewell Cakes'
+      },
+      {
+        path:'../../../assets/mainimg/pastries.jpeg',
+        picHeader: 'Mouth Watering Pastries'
+      }, 
+      {
+        path:'../../../assets/mainimg/birthday.jpeg',
+        picHeader: 'Birthday Cakes'
+      },
+      ];
+
+      }
